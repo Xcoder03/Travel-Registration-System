957573cae0f61d090a72e4bd1ee68c10df7831c4
Xcoder <chindaebube03@gmail.com>
2022-07-14 13:07:17 +0100

    Created bus package

---
+++ src/bus/BusRegistration.java
@@ -0,0 +1,8 @@
+package bus;
+
+public interface BusRegistration {
+    public abstract void signUp(SignUp signup);
+    public abstract void busBooking();
+
+
+}
---
+++ src/bus/SignUp.java
@@ -0,0 +1,49 @@
+package bus;
+
+public class SignUp {
+    private String firstname;
+    private String lastname;
+    private String sex;
+    private String email;
+    private String phoneNo;
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNo() {
+        return phoneNo;
+    }
+
+    public void setPhoneNo(String phoneNo) {
+        this.phoneNo = phoneNo;
+    }
+}
